5c8ccc20162e05b13f80819177caad538c7ce09d
wuxun <wuxunyidao@gmail.com>
2016-02-29 20:26:18 +0800

    Invert Binary Tree

---
+++ src/Invert Binary Tree/Invert Binary Tree.cc
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root != NULL) {
+            TreeNode *left = root->left;
+            TreeNode *right = root->right;
+            root->left = invertTree(right);
+            root->right = invertTree(left);
+        }
+        return root;
+    }
+};
